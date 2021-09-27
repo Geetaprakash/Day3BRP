3d3be80f0b798ecd2d0c90fbc2a7ad8764578748
Geetaprakash <86463646+Geetaprakash@users.noreply.github.com>
2021-09-27 08:49:40 +0530

    Update WindChill.java

--- WindChill.java
+++ WindChill.java
@@ -6,7 +6,7 @@ public class WindChill {
 		double temperature = input.nextDouble();
 		System.out.print("Enter the wind speed (>= 2) in miles per hour: ");
 		double speed = input.nextDouble();
-		// Compute the wind chill index
+		// Compute 
 		double windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temperature * Math.pow(speed, 0.16);
 
 		// Display result
